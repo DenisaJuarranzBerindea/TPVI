3ab8fc7a6bf6556b6079f251960be1c0075c5087
amielrm4 <amiel42004@gmail.com>
2024-10-15 20:30:11 +0200

    cambios en proyecto

--- proyectoSDL1/HolaSDL/Game.h
+++ proyectoSDL1/HolaSDL/Game.h
@@ -1,4 +1,8 @@
 #pragma once
+#include <array>
+
+using uint = unsigned int; //??
+
 class Game
 {
 	/*
@@ -6,5 +10,12 @@ class Game
 	* "un string con una s tras las comillas es constante "s
 	* 
 	*/
+public: 
+	enum TextureName {
+		FONDO, PERRO, HELICOPTERO, NUM_TEXTURES
+	};
+
+private:
+	
 };
 
